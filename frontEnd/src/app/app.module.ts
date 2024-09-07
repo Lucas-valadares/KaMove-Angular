03ca78952838353b085ea2d43b478befa563197b
lucasvasconcelo78 <78914472+Lucas-valadares@users.noreply.github.com>
2024-09-07 00:57:49 -0300

    Fix: criando o projeto e arrumando bugs de versionamento.

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//angular Material
+import { FormsModule } from '@angular/forms'
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+
+    // Angular Material
+    MatCheckboxModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
