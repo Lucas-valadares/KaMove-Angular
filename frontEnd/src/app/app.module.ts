13613c1bf805fe729f9bdce0598fc6abddfb45cf
lucasvasconcelo78 <78914472+Lucas-valadares@users.noreply.github.com>
2024-09-28 05:14:33 -0300

    Feat: Criando o cabeçalho do site.
    Feat: Criando o cabeçalho do site, colocando os 3 botões principais, a logo, nome do site "KaMove", colocando uma animação de caminhão.

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -5,26 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-//angular Material
-import { FormsModule } from '@angular/forms'
-import { MatCheckboxModule } from '@angular/material/checkbox';
 
+//Angular Material
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/template/header/header.component'
+import {MatToolbarModule} from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    AppRoutingModule,
+    AppRoutingModule,  
     BrowserModule,
     BrowserAnimationsModule,
-
-    // Angular Material
-    MatCheckboxModule,
+    
+    //Angular Material
     FormsModule,
+    MatToolbarModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
